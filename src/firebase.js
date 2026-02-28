e2cf1f0f50e6413e8a243f02513474b030eb27e4
liamonaaa <liamonaaa@users.noreply.github.com>
2026-02-28 13:25:58 +0200

    Set firebaseConfig in src/firebase.js to production project values

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAWGRXQ0xwukf8mAnwR48p8mNtP9psE68',
-  authDomain: 'itziks-cart.firebaseapp.com',
-  projectId: 'itziks-cart',
-  storageBucket: 'itziks-cart.firebasestorage.app',
-  messagingSenderId: '94711142380',
-  appId: '1:94711142380:web:86473cdcc467b819da92c9',
-  measurementId: 'G-8Z6KCK64E0',
+  apiKey: "AIzaSyAWGRXQ0xwukf8mAnwR48p8mNtP9psE68",
+  authDomain: "itziks-cart.firebaseapp.com",
+  projectId: "itziks-cart",
+  storageBucket: "itziks-cart.firebasestorage.app",
+  messagingSenderId: "94711142380",
+  appId: "1:94711142380:web:86473cdcc467b819da92c9",
+  measurementId: "G-8Z6KCK64E0"
 };
 
 const app = initializeApp(firebaseConfig);
